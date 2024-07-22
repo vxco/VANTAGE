369ec915660a0a5a1facd8edcbb8807237e45d90
Alfa <norwegianbagel@gmail.com>
2024-07-22 11:57:07 -0700

    added second bound box

--- venv/main.py
+++ venv/main.py
@@ -26,26 +26,29 @@ def list_ports():
         dev_port += 1
     return available_ports, working_ports, non_working_ports
 
+
 def cEd(frame):
     '''Uses Canny Edge Detection, from a blurred input to detect the outline of the cells.'''
     blurred = cv2.GaussianBlur(src=frame, ksize=(3, 5), sigmaX=0.8)
     edges = cv2.Canny(image=blurred, threshold1=100, threshold2=200)
     return blurred, edges
 
+
 def main():
     aspectWidth = 73
     aspectHeight = 27
     aspectMultiplier = 16
-    if aspectMultiplier%2 == 1:
-        print(f"Aspect multiplier was {aspectMultiplier}, should be an even number. Reverting to {aspectMultiplier - 1} ")
+    if aspectMultiplier % 2 == 1:
+        print(
+            f"Aspect multiplier was {aspectMultiplier}, should be an even number. Reverting to {aspectMultiplier - 1} ")
         aspectMultiplier -= 1
 
     imgWidth = aspectWidth * aspectMultiplier
     imgHeight = aspectHeight * aspectMultiplier
 
     cap = cv2.VideoCapture(0)
-    cap.set(3,imgWidth)
-    cap.set(4,imgHeight)
+    cap.set(3, imgWidth)
+    cap.set(4, imgHeight)
     #capturing the image, setting to a 73:27 Aspect ratio, (as the image input suggests by the capillery size)
 
     while True:
@@ -61,7 +64,7 @@ def main():
         erosion = cv2.erode(dilation, rect, iterations=1)
         #cv2.imshow("debug blur", blurred)
         #cv2.imshow("debug edges", edges)
-        cv2.imshow("debug fill",erosion)
+        cv2.imshow("debug fill", erosion)
         '''Color Detection for bounds Setup'''
         #Height Setup
         boundaryHeight = aspectMultiplier * 6
@@ -73,36 +76,32 @@ def main():
         secondaryBoundaryWidth = aspectMultiplier * 8
         tubePad = aspectMultiplier * 5
 
-        initialBoundaryWidth = (aspectWidth - (initialBoundaryStartingPad/aspectMultiplier) - ((tubePad/aspectMultiplier)+(secondaryBoundaryWidth/aspectMultiplier))) * aspectMultiplier
+        initialBoundaryWidth = (aspectWidth - (initialBoundaryStartingPad / aspectMultiplier) - (
+                (tubePad / aspectMultiplier) + (secondaryBoundaryWidth / aspectMultiplier))) * aspectMultiplier
 
-        heightCheck = (2*(boundaryHeight/aspectMultiplier) + 2*(upDownPad/aspectMultiplier) + middlePad/aspectMultiplier)
+        heightCheck = (2 * (boundaryHeight / aspectMultiplier) + 2 * (upDownPad / aspectMultiplier) + middlePad / aspectMultiplier)
         if heightCheck != aspectHeight:
             exit("Err: STP001 - refer to the manual for troubleshooting")
         if initialBoundaryWidth < secondaryBoundaryWidth:
             exit("Err: STP002 - refer to the manual for troubleshooting")
 
-        r2dx1, r2dy1 =  (initialBoundaryStartingPad + initialBoundaryWidth), imgHeight-(upDownPad + boundaryHeight)
-        r2dx2, r2dy2 = (r2x1+secondaryBoundaryWidth), (imgHeight - upDownPad)
+        r2dx1, r2dy1 = (initialBoundaryStartingPad + initialBoundaryWidth), imgHeight - (upDownPad + boundaryHeight)
+        r2dx2, r2dy2 = (r2dx1 + secondaryBoundaryWidth), (imgHeight - upDownPad)
 
-        r2ux1, r2uy1 = initialBoundaryStartingPad,()
+        r2ux1, r2ux2 = r2dx1, r2dx2
 
+        r2uy1, r2uy2 = (r2dy1-116),(r2dy2-116)
 
         #lowerSecondaryRegion = lowerSecondaryRegion[r2y1:r2y2, r2x1:r2x2]
 
-        debugBox = cv2.rectangle(erosion, (int(r2dx1), int(r2dy1)), (int(r2dx2), int(r2dy2)), (255,0,0), 2)
+        debugBox = cv2.rectangle(erosion, (int(r2dx1), int(r2dy1)), (int(r2dx2), int(r2dy2)), (255, 0, 0), 2)
+        cv2.rectangle(debugBox, (int(r2ux1) , int(r2uy1)) , (int(r2ux2), int(r2uy2)), (255,0,0),2)
         cv2.imshow("debug boundbox", debugBox)
-
-
-
-
-
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
     cv2.destroyAllWindows()
-
-
 
 
 if __name__ == "__main__":
