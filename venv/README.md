3f0e9e30217d8a3f9c910989f70f8f45c09e93f5
Alfa <norwegianbagel@gmail.com>
2024-07-22 11:43:08 -0700

    Initial Commit of Code

---
+++ venv/README.md
@@ -0,0 +1,23 @@
+# ACSS User Manual
+# Table of Contents
+1. [Outline](#outline)
+2. [Setup](#setup)
+3. [Error Codes](#error-codes)
+
+## Outline
+This user manual will help you setup, utilize and identify the software ACSS (Automated Cell Sorting System)
+
+## Setup
+
+## Error Codes
+There are three categories of errors, with their respected prefix.
+
+|  Prefix  | Description                                            |
+|:--------:|--------------------------------------------------------|
+|   STP    | Problems in the initialization phase.                  |
+|   CPT    | Problems in the image input                            |
+|   EDL    | Problems in edge detection and cell location detection |
+|   MLM    | Problems in the machine learning model                 |
+|   LVC    | Problems in output or Labview Link                     |
+
+Refer to the table below for sorting out error codes.
---
+++ venv/main.py
@@ -0,0 +1,109 @@
+import cv2
+import numpy as np
+
+
+def list_ports():
+    '''Debug for listing available ports'''
+    non_working_ports = []
+    dev_port = 0
+    working_ports = []
+    available_ports = []
+    while len(non_working_ports) < 6:  # if there are more than 5 non working ports stop the testing.
+        camera = cv2.VideoCapture(dev_port)
+        if not camera.isOpened():
+            non_working_ports.append(dev_port)
+            print("Port %s is not working." % dev_port)
+        else:
+            is_reading, img = camera.read()
+            w = camera.get(3)
+            h = camera.get(4)
+            if is_reading:
+                print("Port %s is working and reads images (%s x %s)" % (dev_port, h, w))
+                working_ports.append(dev_port)
+            else:
+                print("Port %s for camera ( %s x %s) is present but does not reads." % (dev_port, h, w))
+                available_ports.append(dev_port)
+        dev_port += 1
+    return available_ports, working_ports, non_working_ports
+
+def cEd(frame):
+    '''Uses Canny Edge Detection, from a blurred input to detect the outline of the cells.'''
+    blurred = cv2.GaussianBlur(src=frame, ksize=(3, 5), sigmaX=0.8)
+    edges = cv2.Canny(image=blurred, threshold1=100, threshold2=200)
+    return blurred, edges
+
+def main():
+    aspectWidth = 73
+    aspectHeight = 27
+    aspectMultiplier = 16
+    if aspectMultiplier%2 == 1:
+        print(f"Aspect multiplier was {aspectMultiplier}, should be an even number. Reverting to {aspectMultiplier - 1} ")
+        aspectMultiplier -= 1
+
+    imgWidth = aspectWidth * aspectMultiplier
+    imgHeight = aspectHeight * aspectMultiplier
+
+    cap = cv2.VideoCapture(0)
+    cap.set(3,imgWidth)
+    cap.set(4,imgHeight)
+    #capturing the image, setting to a 73:27 Aspect ratio, (as the image input suggests by the capillery size)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            exit("Err: CPT001 - refer to the manual for troubleshooting")
+
+        '''Uses Dilation and Erosion algorithms to fill the detected cell edges, for better location detection of cells.'''
+        blurred, edges = cEd(frame)
+        _, thresh = cv2.threshold(edges, 100, 255, cv2.THRESH_BINARY)
+        rect = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+        dilation = cv2.dilate(thresh, rect, iterations=5)
+        erosion = cv2.erode(dilation, rect, iterations=1)
+        #cv2.imshow("debug blur", blurred)
+        #cv2.imshow("debug edges", edges)
+        cv2.imshow("debug fill",erosion)
+        '''Color Detection for bounds Setup'''
+        #Height Setup
+        boundaryHeight = aspectMultiplier * 6
+        middlePad = aspectMultiplier * 7
+        upDownPad = aspectMultiplier * 4
+
+        #Width Setup
+        initialBoundaryStartingPad = aspectMultiplier * 36
+        secondaryBoundaryWidth = aspectMultiplier * 8
+        tubePad = aspectMultiplier * 5
+
+        initialBoundaryWidth = (aspectWidth - (initialBoundaryStartingPad/aspectMultiplier) - ((tubePad/aspectMultiplier)+(secondaryBoundaryWidth/aspectMultiplier))) * aspectMultiplier
+
+        heightCheck = (2*(boundaryHeight/aspectMultiplier) + 2*(upDownPad/aspectMultiplier) + middlePad/aspectMultiplier)
+        if heightCheck != aspectHeight:
+            exit("Err: STP001 - refer to the manual for troubleshooting")
+        if initialBoundaryWidth < secondaryBoundaryWidth:
+            exit("Err: STP002 - refer to the manual for troubleshooting")
+
+        r2dx1, r2dy1 =  (initialBoundaryStartingPad + initialBoundaryWidth), imgHeight-(upDownPad + boundaryHeight)
+        r2dx2, r2dy2 = (r2x1+secondaryBoundaryWidth), (imgHeight - upDownPad)
+
+        r2ux1, r2uy1 = initialBoundaryStartingPad,()
+
+
+        #lowerSecondaryRegion = lowerSecondaryRegion[r2y1:r2y2, r2x1:r2x2]
+
+        debugBox = cv2.rectangle(erosion, (int(r2dx1), int(r2dy1)), (int(r2dx2), int(r2dy2)), (255,0,0), 2)
+        cv2.imshow("debug boundbox", debugBox)
+
+
+
+
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+
+if __name__ == "__main__":
+    main()
